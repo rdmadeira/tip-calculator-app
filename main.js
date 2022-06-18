d3b5fd49c2c1fab81bdba0e237a653e158bc08c0
rdmadeira <rdmadeira2@gmail.com>
2022-06-18 14:44:59 -0300

    se agrego custom required mensajes con condiciones javascript en number of people

--- main.js
+++ main.js
@@ -16,16 +16,30 @@ window.onload = () => {
         let tipValue;
         let tipInputs = document.getElementsByName('select-tip');
         tipInputs.forEach( item => item.checked ? tipValue = item.value : tipValue );
-        const people = document.getElementById('number-of-people').value;
+        let people = document.getElementById('number-of-people').value;
+        people = Number(people);
+        const requiredNumPos = document.getElementById('required-num');
         const resultTipDiv = document.getElementById('result-tip-amount');
         const resultTotalDiv = document.getElementById('result-total');
         const resultTip = Number(bill) * (Number(tipValue)/100) / Number(people,10);
-        resultTipDiv.textContent = resultTip.toFixed(2);
         const resultTotal = Number(bill) / Number(people) + resultTip;
-        resultTotalDiv.textContent = resultTotal.toFixed(2);
+        resultTipDiv.textContent = '$0.00'
+        resultTotalDiv.textContent = '$0.00'
+        if(people===0 ||people==''){
+            requiredNumPos.textContent = 'Can´t be zero!';
+            document.getElementById('number-of-people').style.outline = 'solid hsl(0deg 76% 52%) 2px';
+        } else if(people < 0 || isNaN(people)) {
+            requiredNumPos.textContent = 'Only (+) numbers!';
+            document.getElementById('number-of-people').style.outline = 'solid hsl(0deg 76% 52%) 2px';
+        } else {
+            requiredNumPos.textContent = '✅';
+            document.getElementById('number-of-people').style.outline = 'solid var(--Strong_cyan) 2px';
+            resultTipDiv.textContent = '$'+resultTip.toFixed(2);
+            resultTotalDiv.textContent = '$'+resultTotal.toFixed(2);
+        }
         console.log(bill);
         console.log(tipValue);
-        console.log(people)
+        console.log(people);
     } 
 
 }
